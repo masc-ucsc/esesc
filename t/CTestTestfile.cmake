18b5aaa8e602c06e8a1b2c061af605d28757b1ef
David Goodman <dagoodma@gmail.com>
2014-05-27 13:48:06 -0700

    Initial unit test commit.

---
+++ t/CTestTestfile.cmake
@@ -0,0 +1,8 @@
+# CMake generated Testfile for 
+# Source directory: /home/dagoodma/Documents/ESESC/esesc/t
+# Build directory: /home/dagoodma/Documents/ESESC/esesc/t
+# 
+# This file includes the relevent testing commands required for 
+# testing this directory and lists subdirectories to be tested as well.
+ADD_TEST(OoOProcessor_unittest "OoOProcessor_unittest")
+SUBDIRS(gtest-1.7.0)
---
+++ t/gtest-1.7.0/CMakeFiles/gtest.dir/DependInfo.cmake
@@ -0,0 +1,13 @@
+# The set of languages for which implicit dependencies are needed:
+SET(CMAKE_DEPENDS_LANGUAGES
+  "CXX"
+  )
+# The set of files for implicit dependencies of each language:
+SET(CMAKE_DEPENDS_CHECK_CXX
+  "/home/dagoodma/Documents/ESESC/esesc/t/gtest-1.7.0/src/gtest-all.cc" "/home/dagoodma/Documents/ESESC/esesc/t/gtest-1.7.0/CMakeFiles/gtest.dir/src/gtest-all.cc.o"
+  )
+SET(CMAKE_CXX_COMPILER_ID "GNU")
+
+# Targets to which this target links.
+SET(CMAKE_TARGET_LINKED_INFO_FILES
+  )
