145f097152802544f7f1a36cf9d27ad234aaa0df
Nursultan Nuridenuly Kabylkas <nkabylka@mada0.SOE.UCSC.EDU>
2018-04-27 16:48:19 -0700

    instruction window tracking and chekcing git push

--- simu/libcore/wavesnap.cpp
+++ simu/libcore/wavesnap.cpp
@@ -1,89 +1,107 @@
 #include "wavesnap.h"
 
 wavesnap::wavesnap() {
-  dump_num = 0;
-  second_edges_added = 0;
-  second_edges_skipped = 0;
+  this->total_count = 0;
+  std::vector<float> temp;
+  temp.push_back(0.0);
+  temp.push_back(0.0);
+  temp.push_back(0.0);
+  temp.push_back(0.0);
+  temp.push_back(0.0);
+  temp.push_back(0.0);
+  temp.push_back(0.0);
+  temp.push_back(0.0);
+  for (uint8_t w=W_LOWER; w<W_UPPER; w++) {
+    IPCs[w];
+    IPCs[w] = temp;
+  }
 }
 
 wavesnap::~wavesnap() {
   //nothing to do here
 }
 
-void wavesnap::addNode(instructionInfo node) {
-  nodeGraph[node.id];
-  
-  if (!nodeGraph[node.id].valid) {
-    nodeGraph[node.id].valid = true;
-    nodeGraph[node.id].info = node;
-  }
-  
-  if (nodeGraph[node.parent1_id].valid) {
-    nodeGraph[node.parent1_id].dependencies.push_back(node.id);
-  }
+void wavesnap::update_window(DInst* dinst) {
+  for (uint32_t i=0; i<MAX_MOVING_GRAPH_NODES; i++) {
+    if (dinst->getID()>=i) {
+      uint64_t id = dinst->getID()-i;
+      windows[id];
+      if (!windows[id].is_set()) {
+        windows[id].set_start_id(id);
+      }
 
+      windows[id].update(dinst);
 
-  if (nodeGraph[node.parent2_id].valid) {
-    nodeGraph[node.parent2_id].dependencies.push_back(node.id);
-  }
-}
-
-void wavesnap::addNode(uint32_t v1, uint32_t v2) {
-  connGraph[v1];
-  connGraph[v1].push_back(v2);
-}
-
-void wavesnap::addEdge(uint32_t v1, uint32_t v2, uint32_t v1_opcode, uint32_t v1_depth) {
-  edge e;
-  e.v1 = v1;
-  e.v2 = v2;
-  e.v1_opcode = v1_opcode;
-  e.v1_depth = v1_depth;
-  addNode(v1, v2);
-  this->edgeList.push_back(e);
-}
-
-bool wavesnap::connected(uint32_t v1, uint32_t v2, uint32_t distance) {
-  bool result = false;
-  std::vector<uint32_t> q;
-  std::set<uint32_t> visited;
-  q.push_back(v1);
-
-  for (uint32_t i=0; i<q.size() && q.size()<distance; i++) {
-    uint32_t process = q[i];
-    for (uint32_t j=0; j<connGraph[process].size(); j++) {
-      if (connGraph[process][j] != v2) {
-        if (visited.find(connGraph[process][j])==visited.end()) {
-          q.push_back(connGraph[process][j]);
-          visited.insert(connGraph[process][j]);
+      if (windows[id].get_size() == MAX_MOVING_GRAPH_NODES) {
+        if (windows[id].min_fetch_time == 0) {
+          windows[id].dump();
         }
-      } else {
-        result = true;
-        break;
+        uint64_t address = windows[id].get_first_inst().getPC();
+        window_info* w_a = &window_address_info[address];
+        
+        w_a->count++;
+
+        pipeline_info next;
+        next.count = 1;
+        std::string encode = "";
+        for (uint32_t i=0; i<MAX_MOVING_GRAPH_NODES; i++) {
+          if (windows[id].check_if_control(i)) {
+            continue;
+          }
+
+
+          std::vector<int32_t> temp = windows[id].get_pipeline_info(i);
+          next.wait_cycles.push_back(temp[0]);
+          next.rename_cycles.push_back(temp[1]);
+          next.issue_cycles.push_back(temp[2]);
+          next.execute_cycles.push_back(temp[3]);
+          next.instructions.push_back(temp[4]);
+          encode += ENCODING[temp[4]];
+        }
+        w_a->p_i[encode];
+        pipeline_info *temp = &(w_a->p_i[encode]);
+        if (temp->instructions.size() == 0) {
+          *temp = next;
+        } else {
+          temp->count++;
+          for (uint32_t i=0; i<temp->instructions.size(); i++) {
+            temp->instructions[i]+=next.instructions[i];
+            temp->wait_cycles[i]+=next.wait_cycles[i];
+            temp->rename_cycles[i]+=next.rename_cycles[i];
+            temp->issue_cycles[i]+=next.issue_cycles[i];
+            temp->execute_cycles[i]+=next.execute_cycles[i];
+          }
+        }
+
+        windows.erase(id);
       }
     }
-    if (result) {
-      break;
-    }
   }
-  q.clear(); 
-  visited.clear();
-  return result;
 }
 
-void wavesnap::add(DInst* dinst) {
-  uint32_t dst, src1, src2;
+
+void wavesnap::add_to_RAT(DInst* dinst) {
+  uint64_t dst, src1, src2;
   bool skip = false;
   if (dinst->getInst()->isStore()) {
-    dst =  ((uint32_t)dinst->getAddr()%HASH_SIZE)+REGISTER_NUM;
+    //dst =  ((uint64_t)dinst->getAddr()%HASH_SIZE)+REGISTER_NUM;
+    dst =  ((uint64_t)dinst->getAddr())+REGISTER_NUM;
     src1 = (uint32_t)dinst->getInst()->getSrc1();
     src2 = (uint32_t)dinst->getInst()->getSrc2();
   } else if (dinst->getInst()->isLoad()) {
     dst =  (uint32_t)dinst->getInst()->getDst1();
     src1 = (uint32_t)dinst->getInst()->getSrc1();
-    src2 = ((uint32_t)dinst->getAddr()%HASH_SIZE)+REGISTER_NUM;
+    src2 = ((uint64_t)dinst->getAddr())+REGISTER_NUM;
   } else if (dinst->getInst()->isControl()) {
     //todo
+    dst =  (uint32_t)dinst->getInst()->getDst1();
+    src1 = (uint32_t)dinst->getInst()->getSrc1();
+    src2 = (uint32_t)dinst->getInst()->getSrc2();
+    /*
+    std::cout << dinst->getInst()->getOpcodeName() << " ";
+    std::cout << dst << ",";
+    std::cout << src1 << ",";
+    std::cout << src2 << "," << std::endl;*/
     skip = true;
   } else {
     dst =  (uint32_t)dinst->getInst()->getDst1();
@@ -91,86 +109,221 @@ void wavesnap::add(DInst* dinst) {
     src2 = (uint32_t)dinst->getInst()->getSrc2();
   }
 
-  if (!skip) {
+  if (false) {
     int32_t p1_depth = 0;
     int32_t p2_depth = 0;
 
+    RAT[dst];
     RAT[dst].valid = true;
-    RAT[dst].id = (uint32_t)dinst->getID();
+    RAT[dst].id = (uint32_t)dinst->getID()+1;
     RAT[dst].dst = dst;
     RAT[dst].src1 = src1;
     RAT[dst].src2 = src2;
     RAT[dst].opcode_name = dinst->getInst()->getOpcodeName();
     RAT[dst].opcode = (uint32_t)dinst->getInst()->getOpcode();
-    if (RAT[src1].valid) {
-      RAT[dst].parent1_id = RAT[src1].id;
-      p1_depth = RAT[src1].depth+1;
-    }
-    if (RAT[src2].valid) {
-      RAT[dst].parent2_id = RAT[src2].id;
-      p2_depth = RAT[src2].depth+1;
-    }
-
-    if (p1_depth>p2_depth) {
-      RAT[dst].depth = p1_depth; 
-      addEdge(RAT[dst].id, RAT[dst].parent1_id, RAT[dst].opcode, RAT[dst].depth);
-      if (!connected(RAT[dst].parent1_id, RAT[dst].parent2_id, RAT[dst].depth-p2_depth)) {
-        addEdge(RAT[dst].id, RAT[dst].parent2_id, RAT[dst].opcode, RAT[dst].depth);
-        second_edges_added++;
-      } else {
-        second_edges_skipped++;
-      }
-    } else {
-      RAT[dst].depth = p2_depth; 
-      addEdge(RAT[dst].id, RAT[dst].parent2_id, RAT[dst].opcode, RAT[dst].depth);
-      if (!connected(RAT[dst].parent2_id, RAT[dst].parent1_id, RAT[dst].depth-p1_depth)) {
-        addEdge(RAT[dst].id, RAT[dst].parent1_id, RAT[dst].opcode, RAT[dst].depth);
-        second_edges_added++;
-      } else {
-        second_edges_skipped++;
-      }
-    }
-
-
-    //addNode(RAT[dst]);
-
+    RAT[dst].time_stamp = (uint64_t)globalClock;
   }
 }
 
-void wavesnap::dumpGraph(std::string fileName) {
+void wavesnap::merge() {
+/*
+  std::vector<uint64_t> merge_candidates;
   uint32_t i = 0;
-  uint32_t j = 0;
-  std::cout << "Dumping graph..." << std::endl;
+  for (auto& kv:window_address_info) {
+    if (merge_candidates.size() == 0) {
+      merge_candidates.push_back(kv.first);
+    } else {
+      uint32_t diff = kv.first-merge_candidates[merge_candidates.size()-1];
+      if (diff==4) {
+        merge_candidates.push_back(kv.first);
+      } else {
+        //process the candidates
+  
+        //finally clear the candidates
+        merge_candidates.clear();
+        merge_candidates.push_back(kv.first);
+      }
+    }
+  }
+*/
+}
+
+float max_average(uint8_t w, std::map<uint32_t, uint32_t>* cycles) {
+  uint32_t moving_sum = 0;
+  float max_ave = 0;
+  std::vector<uint32_t> cycles_v;
+  for (auto& kv: *cycles) {
+    cycles_v.push_back(kv.second);
+  }
+
+  for (uint16_t i=0; i<w; i++) {
+    moving_sum+=cycles_v[i];
+  }
+
+  max_ave = moving_sum/w;
+  for (uint16_t i=w; i<cycles_v.size(); i++) {
+    moving_sum += cycles_v[i];
+    moving_sum -= cycles_v[i-w];
+    float ave = moving_sum/w;
+    if (ave>max_ave) {
+      max_ave = ave;
+    }
+  }
+
+  return max_ave;
+}
+
+void wavesnap::calculate_ipc() {
+  float fetched_ipc = 0.0;
+  float renamed_ipc = 0.0;
+  float issued_ipc = 0.0;
+  float executed_ipc = 0.0;
+  float fetched_diff = 0.0;
+  float renamed_diff = 0.0;
+  float issued_diff = 0.0;
+  float executed_diff = 0.0;
+  for (auto& kv:window_address_info) {
+    //control. this will dramatically reduce amound of calculation
+    if (kv.second.count>COUNT_ALLOW) {
+      this->total_count+=kv.second.count;
+      if (kv.second.p_i.size()>0) {
+        for (auto& a_pi:kv.second.p_i) {
+          pipeline_info* current = &a_pi.second;
+          std::map<uint32_t, uint32_t> cycles_fetched;
+          std::map<uint32_t, uint32_t> cycles_renamed;
+          std::map<uint32_t, uint32_t> cycles_issued;
+          std::map<uint32_t, uint32_t> cycles_executed;
+          cycles_fetched.clear();
+          cycles_renamed.clear();
+          cycles_issued.clear();
+          cycles_executed.clear();
+          for (uint16_t i=0; i<current->instructions.size(); i++) {
+            uint32_t finished = 0;
+            finished += std::floor(current->wait_cycles[i]/current->count);
+            cycles_fetched[finished];
+            cycles_fetched[finished]++;
+
+            finished += std::floor(current->rename_cycles[i]/current->count);
+            cycles_renamed[finished];
+            cycles_renamed[finished]++;
+
+            finished += std::floor(current->issue_cycles[i]/current->count);
+            cycles_issued[finished];
+            cycles_issued[finished]++;
+
+            finished += std::floor(current->execute_cycles[i]/current->count);
+            cycles_executed[finished];
+            cycles_executed[finished]++;
+          }
+
+          //calculate ipc for particular window
+          for (uint8_t w=W_LOWER; w<W_UPPER; w++) {
+            //retrieve
+            fetched_ipc = this->IPCs[w][0];
+            fetched_diff = this->IPCs[w][1];
+            renamed_ipc = this->IPCs[w][2];
+            renamed_diff = this->IPCs[w][3]; 
+            issued_ipc = this->IPCs[w][4];
+            issued_diff = this->IPCs[w][5]; 
+            executed_ipc = this->IPCs[w][6];
+            executed_diff = this->IPCs[w][7]; 
+            //calculate
+            float temp_ave;
+            uint64_t c = current->count;
+            temp_ave = max_average(w, &cycles_fetched);
+            fetched_ipc += std::ceil(temp_ave)*c;
+            fetched_diff += (std::ceil(temp_ave)-temp_ave)*c;
+
+            temp_ave = max_average(w, &cycles_renamed);
+            renamed_ipc += std::ceil(temp_ave)*c;
+            renamed_diff += (std::ceil(temp_ave)-temp_ave)*c;
+
+            temp_ave = max_average(w, &cycles_issued);
+            issued_ipc += std::ceil(temp_ave)*c;
+            issued_diff += (std::ceil(temp_ave)-temp_ave)*c;
+
+            temp_ave = max_average(w, &cycles_executed);
+            executed_ipc += std::ceil(temp_ave)*c; 
+            executed_diff += (std::ceil(temp_ave)-temp_ave)*c;
+            //put back
+            this->IPCs[w][0] = fetched_ipc;
+            this->IPCs[w][1] = fetched_diff;
+            this->IPCs[w][2] = renamed_ipc;
+            this->IPCs[w][3] = renamed_diff;
+            this->IPCs[w][4] = issued_ipc;
+            this->IPCs[w][5] = issued_diff;
+            this->IPCs[w][6] = executed_ipc;
+            this->IPCs[w][7] = executed_diff;
+          }
+        }
+      }
+    }
+  }
+}
+
+
+std::string wavesnap::dump_w() {
+  std::stringstream output;
+  output << std::setw(10) << "width";
+  output << std::setw(10) << "fetch";
+  output << std::setw(10) << "rename";
+  output << std::setw(10) << "issue";
+  output << std::setw(10) << "execute";
+  output << std::endl;
+  for (uint8_t w=W_LOWER; w<W_UPPER; w++) {
+    float fetch_ipc = (IPCs[w][0]-IPCs[w][1])/this->total_count;
+    float rename_ipc = (IPCs[w][2]-IPCs[w][3])/this->total_count;
+    float issue_ipc = (IPCs[w][4]-IPCs[w][5])/this->total_count;
+    float execute_ipc = (IPCs[w][6]-IPCs[w][7])/this->total_count;
+    output << std::setw(10) << (int)w;
+    output << std::setw(10) << fetch_ipc;
+    output << std::setw(10) << rename_ipc;
+    output << std::setw(10) << issue_ipc;
+    output << std::setw(10) << execute_ipc;
+    output << std::endl;
+  }
+
+  return output.str();
+}
+
+void wavesnap::dump_signatures(std::string fileName) {
+  std::cout << "Dumping window signatures..." << std::endl;
   std::ofstream output;
   output.open(fileName);
-  for (const auto &p : nodeGraph) {
-      output << p.second.info.id << " ";
-      output << p.second.info.opcode << " ";
-      for (int i=1; i<p.second.dependencies.size(); i++) {
-        output << p.second.dependencies[i] << " ";
-      }
-      output << std::endl;
-      i++;
-      if (i%MAX_NODE_NUM==0) {
-        j++;
-        output.close();
-        output.open(fileName+std::to_string(j));
-      }
+  for (auto& kv:window_sign_info) {
+    output << kv.second << " ";
+    output << kv.first << std::endl;
   }
+
   output.close();
 }
 
-void wavesnap::dumpEdgeList(std::string fileName) {
-  std::cout << "Dumping edge list..." << std::endl;
+void wavesnap::dump_address(std::string fileName) {
+  std::cout << "Dumping window addresses..." << std::endl;
   std::ofstream output;
-  output.open(fileName+"list"+std::to_string(dump_num));
-  for (uint32_t i=0; i<edgeList.size(); i++) {
-    output << edgeList[i].to_string() << std::endl;
+  output.open(fileName);
+  output << dump_w() << std::endl;
+  for (auto& kv:window_address_info) {
+    if (kv.second.count>COUNT_ALLOW) {
+      output << kv.first << " ";
+      output << kv.second.count << " ";
+      output << kv.second.p_i.size() << std::endl;
+      if (kv.second.p_i.size()>0) {
+        //output << "+++" << std::endl;
+        for (auto& a_pi:kv.second.p_i) {
+          output << a_pi.first << " " << a_pi.second.count << " ";
+          for (uint16_t i=0; i<a_pi.second.instructions.size(); i++) {
+            output << std::ceil(a_pi.second.wait_cycles[i]/a_pi.second.count) << ".";
+            output << std::ceil(a_pi.second.rename_cycles[i]/a_pi.second.count) << ".";
+            output << std::ceil(a_pi.second.issue_cycles[i]/a_pi.second.count) << ".";
+            output << std::ceil(a_pi.second.execute_cycles[i]/a_pi.second.count) << ".";
+          }
+          output << std::endl;
+        }
+        //output << "+++" << std::endl;
+      }
+    }
   }
-  output.close();
-  edgeList.clear();
-}
 
-void wavesnap::dumpConnGraph() {
-  std::cout << connGraph.size() << std::endl;
+  output.close();
 }
--- simu/libcore/wavesnap.h
+++ simu/libcore/wavesnap.h
@@ -12,17 +12,28 @@
 
 #ifndef _WAVESNAP_
 #define _WAVESNAP_
+//functional defines
+//#define REMOVE_REDUNDANT_EDGES
 
 //signature defines
 #define REGISTER_NUM 32
-#define HASH_SIZE 65536
+#define HASH_SIZE 4*65536
 
 //node list defines
 #define MAX_NODE_NUM 1000
-#define MAX_EDGE_NUM 100000
+#define MAX_EDGE_NUM 1000000
+#define MAX_MOVING_GRAPH_NODES 64
+#define COUNT_ALLOW 9
 
 //dump path
 #define EDGE_DUMP_PATH "./edge_list/"
+
+//encode instructions
+#define ENCODING "0123456789abcdefghijklmnopqrstuvwxyzABCDEFHIJKLMNPQRSTUVWXYZ"
+
+//misc defines
+#define W_LOWER 2
+#define W_UPPER 13
 
 #include <vector>
 #include <stdint.h>
@@ -36,10 +47,11 @@
 #include "InstOpcode.h"
 #include "EmuSampler.h"
 #include "DInst.h"
+#include "BloomFilter.h"
 
 class wavesnap {
   private:
-    class instructionInfo {
+    class instruction_info {
       public:
         uint32_t id;
         uint32_t dst;
@@ -51,8 +63,9 @@ class wavesnap {
         bool valid;
         std::string opcode_name;
         uint32_t opcode;
+        uint64_t time_stamp;
 
-        instructionInfo() {
+        instruction_info() {
           this->id = 0;
           this->dst = 0;
           this->src1 = 0;
@@ -61,6 +74,7 @@ class wavesnap {
           this->parent2_id = 0;
           this->depth = 0;
           this->valid = false;
+          this->time_stamp = 0;
         }
 
         std::string dump() {
@@ -79,69 +93,164 @@ class wavesnap {
         }
     };
 
-    class instruction_node {
+    class pipeline_info {
+      private:
+
       public:
-        instructionInfo info;
-        std::vector<uint32_t> dependencies;
-        bool valid;
- 
-        instruction_node() {
-          this->info.id = 0;
-          this->dependencies.push_back(0);
-          this->valid = false;
+        std::vector<uint8_t> instructions;
+        std::vector<uint32_t> wait_cycles;
+        std::vector<uint32_t> rename_cycles;
+        std::vector<uint32_t> issue_cycles;
+        std::vector<uint32_t> execute_cycles;
+        uint32_t count;
+
+        pipeline_info() {
+          this->count = 0;
         }
     };
 
-    class edge {
-      public:
-        uint32_t v1;
-        uint32_t v2;
-        uint32_t v1_opcode;
-        uint32_t v1_depth;
+    class window_info {
+      private:
 
-        edge() {
-          //nothing to do here
+      public:
+        std::map<std::string, pipeline_info> p_i;
+        uint32_t count;
+
+        window_info() {
+          this->count = 0;
+        }
+        ~window_info() {
+
+        }
+    };
+
+
+    class instruction_window {
+      private:
+        bool check_window[MAX_MOVING_GRAPH_NODES];
+        bool is_control[MAX_MOVING_GRAPH_NODES];
+        uint64_t start_id;
+        bool set = false;
+        uint32_t size;
+
+      public:
+        DInst window[MAX_MOVING_GRAPH_NODES];
+        uint64_t min_fetch_time;
+        instruction_window() {
+          this->set = false;
+          this->size = 0;
+          this->min_fetch_time = std::numeric_limits<uint64_t>::max();
+          for (uint32_t i=0; i<MAX_MOVING_GRAPH_NODES; i++) {
+            check_window[i] = false;
+            is_control[i] = false;
+          }
+        }
+        
+        ~instruction_window() {
+          //nothing todo here
+        }
+
+        DInst get_first_inst() {
+          return this->window[0];
+        }
+
+        bool check_if_control(uint16_t i) {
+          return this->is_control[i];
+        }
+
+        std::vector<int32_t> get_pipeline_info(uint32_t n) {
+          std::vector<int32_t> result;
+          int32_t w,r,i,e;
+          if (n>=0 && n<=MAX_MOVING_GRAPH_NODES) {
+            w = window[n].getFetchedTime()-this->min_fetch_time;
+            r = window[n].getRenamedTime()-window[n].getFetchedTime();
+            i = window[n].getIssuedTime()-window[n].getRenamedTime();
+            e = window[n].getExecutedTime()-window[n].getIssuedTime();
+
+            if (w<0 || r<0 || i<0 || e<0) {
+              std::cout << n << " ";
+              std::cout << window[n].getFetchedTime() << " ";
+              std::cout << this->min_fetch_time << " ";
+              std::cout << window[n].getInst()->getOpcode() << " ";
+              std::cout << window[n].getID() << " ";
+              std::cout << w << "w ";
+              std::cout << r << "r ";
+              std::cout << i << "i ";
+              std::cout << e << "e ";
+              std::cout << std::endl;
+            }
+            result.push_back(w);
+            result.push_back(r);
+            result.push_back(i);
+            result.push_back(e);
+            result.push_back(window[n].getInst()->getOpcode());
+          } else {
+            std::cout << "error: DInst get_pipeline_info: out of window" << std::endl;
+          }
+          return result;
+        }
+
+        bool is_set() {
+          return this->set;
+        }
+
+        uint32_t get_size() {
+          return this->size;
+        }
+
+        void set_start_id(uint64_t start_id) {
+          this->start_id = start_id;
+          this->set = true;
         }
   
-        edge (uint32_t v1, uint32_t v2, uint32_t v1_opcode, uint32_t v1_depth) {
-          this->v1 = v1;
-          this->v2 = v2;
-          this->v1_opcode = v1_opcode;
-          this->v1_depth = v1_depth;
+        void update(DInst* dinst) {
+          //check if this instruction is allowed in this window
+          int32_t window_index = dinst->getID()-(this->start_id);
+          if (window_index>=MAX_MOVING_GRAPH_NODES || window_index<0) {
+            std::cout << "this instruction should not be in this window (" << window_index << "," << this->start_id << ")" << std::endl;
+          } else {
+            window[window_index] = *dinst;
+            if (check_window[window_index] == false) {
+              check_window[window_index] = true;
+              if (dinst->getInst()->isControl()) {
+                is_control[window_index] = true;
+              }
+              size++;
+              if (min_fetch_time>dinst->getFetchTime()) {
+                min_fetch_time = dinst->getFetchTime();
+              }
+            }
+          }
         }
-
-        ~edge() {
-          //nothing to do here
-        }
-
-        std::string to_string() {
-          std::stringstream result;
-          result << v1 << " " << v1_opcode << " " << v1_depth  << " " << v2;
-          return result.str();
+    
+        void dump() {
+          for (uint32_t i=0; i<MAX_MOVING_GRAPH_NODES; i++) {
+            std::cout << window[i].getFetchedTime() << " ";
+            if (window[i].getFetchedTime() == 0) {
+              std::cout << window[i].getInst()->getOpcode();
+            }
+            std::cout << std::endl;
+          }
+          std::cout <<  "**------------**" << std::endl;
         }
     };
 
-    instructionInfo RAT[REGISTER_NUM+HASH_SIZE];
-    std::map<uint32_t, instruction_node> nodeGraph;
-    std::map<uint32_t, std::vector<uint32_t>> connGraph;
-    std::vector<edge> edgeList;
-    void sign(uint32_t depth, uint32_t w);
-    void addNode(instructionInfo node);
-    void addNode(uint32_t v1, uint32_t);
-    void addEdge(uint32_t v1, uint32_t v2, uint32_t v1_opcode, uint32_t v1_depth);
-    bool connected(uint32_t v1, uint32_t v2, uint32_t distance);
+    std::map<uint64_t, instruction_window> windows;
 
   public:
     wavesnap();
     ~wavesnap();
-    void add(DInst* dinst);
-    void dumpGraph(std::string fileName);
-    void dumpConnGraph();
-    void dumpEdgeList(std::string fileName);
-
-    //statistical variables
-    uint32_t dump_num;
-    uint32_t second_edges_added;
-    uint32_t second_edges_skipped;
+    void update_window(DInst* dinst);
+    void add_to_RAT(DInst* dinst);
+    void merge();
+    void calculate_ipc();
+    void dump_signatures(std::string fileName);
+    void dump_address(std::string fileName);
+    uint64_t total_count;
+    std::string dump_w();
+    std::map<std::string, uint64_t> window_sign_info;
+    std::map<uint64_t, window_info> window_address_info;
+    std::map<uint64_t, instruction_info> RAT;
+    std::map<uint8_t, std::vector<float>> IPCs;
 };
 #endif
